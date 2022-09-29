2deec3de170128d67e3c5f3b5fa4aa811b70733b
Rai Osborne <rai.osborne@audacia.co.uk>
2022-09-29 08:56:07 +0100

    feat: all API tests

---
+++ APITestingTemplate/APITestingTemplate/DataSetup/Customizations/BaseCustomizations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using APITestingTemplate.Models.Dtos;
+using Audacia.Random.Extensions;
+using AutoFixture;
+
+namespace APITestingTemplate.DataSetup.Customizations
+{
+    public class BaseCustomizations: ICustomization
+    {
+        private Random Random { get; } = new();
+
+        public void Customize(IFixture fixture)
+        {
+            // Books
+            fixture.Register(() =>
+                fixture.Build<AddBookRequest>()
+                    .With(dto => dto.Title, () => Random.Words(2))
+                    .With(dto => dto.Description, () => Random.Sentence())
+                    .With(dto => dto.Author, () => Random.FemaleForename() + ' ' + Random.Surname())
+                    .With(dto => dto.PublishedYear, () => 2015)
+                    .With(dto => dto.HasEBook, () => true)
+                    .With(dto => dto.AvailableFrom, () => DateTime.Parse("2022-09-16T12:55:22.117Z"))
+                    .With(dto => dto.BookCategoryId, () => 1).Create());
+        }
+    }
+}
---
+++ APITestingTemplate/APITestingTemplate/Tests/BookCategory/AsAUserICanGetAllBookCategorys.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using APITestingTemplate.Fixtures;
+using APITestingTemplate.Helpers;
+using APITestingTemplate.Models.Dtos;
+using Audacia.Testing.Api;
+using FluentAssertions;
+using Xunit;
+
+namespace APITestingTemplate.Tests.BookCategory
+{
+    public class GetAllBookCategoriesTest : ApiTestsBase, IClassFixture<AddMultipleCategoriesFixture>
+    {
+
+
+        private readonly AddMultipleCategoriesFixture _addMultipleCategoriesFixture;
+
+
+        public GetAllBookCategoriesTest(AddMultipleCategoriesFixture addMultipleCategoriesFixture)
+        {
+
+            _addMultipleCategoriesFixture = addMultipleCategoriesFixture;
+
+        }
+
+        [Fact]
+        public void Scenario_12_As_a_user_I_can_get_all_book_categories()
+        {
+
+            //Get category details
+            var getCategoryIdOne = Convert.ToString(_addMultipleCategoriesFixture.BookCategoryData.BookCategoryData.First().Id);
+            var getCategoryIdTwo = Convert.ToString(_addMultipleCategoriesFixture.BookCategoryData.BookCategoryData.ElementAt(1).Id);
+            var getCategoryIdThree = Convert.ToString(_addMultipleCategoriesFixture.BookCategoryData.BookCategoryData.ElementAt(2).Id);
+            var getCategoryIdFour = Convert.ToString(_addMultipleCategoriesFixture.BookCategoryData.BookCategoryData.ElementAt(3).Id);
+            var getCategoryIdFive = Convert.ToString(_addMultipleCategoriesFixture.BookCategoryData.BookCategoryData.ElementAt(4).Id);
+
+
+            // Call the get all book categories
+            var getAllBookCategoriesResponse = GetAll<GetBookCategoryDtoCommandResult>(Resources.GetAllBookCategories, null);
+
+            // Check the status code is ok
+            getAllBookCategoriesResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            // Check that the book category details are correct
+            getAllBookCategoriesResponse.Content.Should().Be(getCategoryIdOne);
+
+            // { APITestingTemplate.Models.Dtos.GetBookDtoIEnumerableCommandResult}
+        }
+    }
+}
